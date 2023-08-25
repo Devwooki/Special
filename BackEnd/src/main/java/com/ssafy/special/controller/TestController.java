bafbfe372ddb99d7da1575804e549935c7dc257c
이현욱 <dldnr789@gmail.com>
2023-08-25 12:49:52 +0900

    Deploy. 배포 테ㅅ스트

---
+++ BackEnd/src/main/java/com/ssafy/special/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.ssafy.special.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api/test")
+public class TestController {
+
+    @GetMapping()
+    @ResponseStatus(HttpStatus.OK)
+    public String apiTest(){
+        return "출력잘 됩니다.";
+    }
+}
