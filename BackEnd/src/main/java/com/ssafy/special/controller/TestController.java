99743c24e99bfa73df850deaa06d8b137a4476c4
이현욱 <dldnr789@gmail.com>
2023-09-01 17:50:19 +0900

    external-nginx 적용

--- BackEnd/src/main/java/com/ssafy/special/controller/TestController.java
+++ BackEnd/src/main/java/com/ssafy/special/controller/TestController.java
@@ -2,12 +2,10 @@ package com.ssafy.special.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/api/test")
+@RestController("/api/test")
 public class TestController {
 
     @GetMapping()
