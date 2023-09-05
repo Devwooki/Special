f984d4f2c9ba097e868e4c4b6ec68ed9b2b7cad9
이현욱 <dldnr789@gmail.com>
2023-09-05 11:04:59 +0900

    fix.spring

--- BackEnd/src/main/java/com/ssafy/special/controller/TestController.java
+++ BackEnd/src/main/java/com/ssafy/special/controller/TestController.java
@@ -2,10 +2,12 @@ package com.ssafy.special.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api/test")
+@RestController
+@RequestMapping("api/test")
 public class TestController {
 
     @GetMapping()
